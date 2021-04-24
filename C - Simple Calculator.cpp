fa421f51701079982cd8cfddc430376455ace826
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-25 00:23:18 +0200

    Create C - Simple Calculator.cpp

---
+++ C - Simple Calculator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);
+   long long x , y, sum , multi , sub;
+   cin >> x >> y ;
+   sum = x + y ;
+   multi = x*y ;
+   sub = x - y ;
+   cout << x << " + " << y << " = " << sum << endl;
+   cout << x << " * " << y << " = " << multi << endl;
+   cout << x << " - " << y << " = " << sub << endl;
+
+}
