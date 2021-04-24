3857f571c8ffc5e38e3de9cd63fba70b64899425
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-24 23:58:16 +0200

    Create B - Basic Data Types.cpp

---
+++ B - Basic Data Types.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i; long long ll ; char c ; float f; double d ;
+    cin >> i >> ll >> c >> f >> d;
+    cout << i <<"\n" << ll <<"\n"<< c <<"\n"<< f <<"\n"<< d;
+    return 0;
+}
