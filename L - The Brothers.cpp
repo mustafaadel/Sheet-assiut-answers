fd716dd0bab2ad386f9303d212a38305a96553a1
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-25 05:37:10 +0200

    Create L - The Brothers.cpp

---
+++ L - The Brothers.cpp
@@ -0,0 +1,14 @@
+            #include <bits/stdc++.h>
+            using namespace std;
+            int main() {
+                ios::sync_with_stdio(0);cin.tie(0);
+                ios_base::sync_with_stdio(0);
+                string f1 ,f2 , s1 , s2;
+                cin >> f1 >> s1;
+                cin >> f2 >> s2 ;
+            
+                if (s1 == s2 )
+                    cout << "ARE Brothers";
+                else
+                    cout << "NOT";
+            }
