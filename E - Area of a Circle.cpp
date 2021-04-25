b637b693a7194b0ee232ee9777c8484c828698d8
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-25 04:10:37 +0200

    Create E - Area of a Circle.cpp

---
+++ E - Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+const double pi = 3.141592653;
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cout << fixed << setprecision(9);
+    double r , area;
+    cin >> r;
+    area = pi*r*r;
+    cout << area;
+
+}
