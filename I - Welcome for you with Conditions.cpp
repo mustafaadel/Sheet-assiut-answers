9269a68856228107b52a94c5298ef744daaece75
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-25 05:07:41 +0200

    Create I - Welcome for you with Conditions.cpp

---
+++ I - Welcome for you with Conditions.cpp
@@ -0,0 +1,14 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main() {
+        ios::sync_with_stdio(0);cin.tie(0);
+        ios_base::sync_with_stdio(0);
+        short int a , b ;
+        cin >> a >> b ;
+        if ( a >= b)
+        {
+            cout << "Yes";
+        }
+        else
+            cout << "No";
+    }
