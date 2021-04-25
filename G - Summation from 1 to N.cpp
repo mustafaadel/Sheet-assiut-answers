80d91283fb14ba41b7326e442dad6bb0f5dacbf3
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-25 04:40:25 +0200

    Create  	G - Summation from 1 to N.cpp

---
+++ G - Summation from 1 to N.cpp
@@ -0,0 +1,13 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+    int main() {
+        ios::sync_with_stdio(0);cin.tie(0);
+        ios_base::sync_with_stdio(0);
+        long long n ,sum ;
+        cin >> n ;
+        sum = n*(n+1)/2;
+
+        cout << sum;
+
+
+    }
