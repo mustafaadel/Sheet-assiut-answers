9242c5c28dd2a6aca8d28750555edf9ed1ddc0ae
Mostafa Adel Zakaria <30378112+mustafaadel@users.noreply.github.com>
2021-04-30 01:09:49 +0200

    Create F - Multiplication table.cpp

---
+++ F - Multiplication table.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define atom ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);ios_base::sync_with_stdio(0);
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+using namespace std;
+int main() {
+   atom;
+    int n ;
+    cin >> n;
+    for (int i = 1 ; i <= 12 ; i++ )
+    {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+    }
+
+
